5bc2a1abeda94993120dc73bf55f3a0d1d2a2547
Zivvvo <zhe.fan@mail.mcgill.ca>
2020-11-04 18:21:53 -0700

    Updated filament_trace.py identify helix id per cluster visited on a micrograph

--- src/filament_trace.py
+++ src/filament_trace.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+
 sys.path.append(".")
 
 import matplotlib.pyplot as plt
@@ -11,7 +12,8 @@ from utils.filament_fit import *
 
 sys.path.append(".")
 
-parser = argparse.ArgumentParser(description="Traces coordinates along filaments, spaced equidistantly after every given pixel value")
+parser = argparse.ArgumentParser(
+    description="Traces coordinates along filaments, spaced equidistantly after every given pixel value")
 
 parser.add_argument("filament_PATH", help="location of filament files")
 
@@ -19,30 +21,33 @@ parser.add_argument("-t", "--threshold", help="threshold for particle extraction
 
 parser.add_argument("save_PATH", help="save location for fitted filament plots")
 
-parser.add_argument("spacing", help="distance between neighbouring particles", type= float)
+parser.add_argument("spacing", help="distance between neighbouring particles", type=float)
 
-parser.add_argument("-eps", "--eps", help = "The maximum distance between two " +
-                                            "samples for one to be considered " +
-                                            "as in the neighborhood of the other. "
-                                            ,type = float)
-parser.add_argument("-im", "--image", help="if specified with 1, will save an image for the coordiantes as png as well",
-                    type = int)
-parser.add_argument("-min_samples", "--min_samples", help = "The number of samples (or total weight) in a neighborhood for a point to be considered as a core point. This includes the point itself."
-                    , type = float)
-parser.add_argument("-box","--box_size", help = "box size of the selected particles", type=int)
+parser.add_argument("-eps", "--eps", nargs= '?', const= 10,help="The maximum distance between two " +
+                                          "samples for one to be considered " +
+                                          "as in the neighborhood of the other. "
+                    , type=float)
+parser.add_argument("-im", "--image", nargs= "?", const=0, help="if specified with 1, will save an image for the coordiantes as png as well",
+                    type=bool)
+parser.add_argument("-min_samples", "--min_samples", nargs = "?", const = 5,
+                    help="The number of samples (or total weight) in a neighborhood for a point to be considered as a core point. This includes the point itself."
+                    , type=float)
+parser.add_argument("-box", "--box_size", nargs= "?", const=100, help="box size of the selected particles", type=int)
+parser.add_argument("-min_part", "--min_part", nargs="?", const=10, help="the minimum number of particles in a cluster/filament", type=int)
 
 args = parser.parse_args()
 
 box_size = args.box_size
 
-#convert to numpy
+min_part = args.min_part
+
+# convert to numpy
 file_library = None
 if args.threshold is not None:
-    file_library = parse_helix_coordinates(args.filament_PATH, threshold = args.threshold)
+    file_library = parse_helix_coordinates(args.filament_PATH, threshold=args.threshold)
     print("args.threshold is provided")
 else:
     file_library = parse_helix_coordinates(args.filament_PATH)
-
 
 for file in file_library:
     print(file)
@@ -55,6 +60,7 @@ for file in file_library:
 
     fig1 = plt.figure(1)
 
+    helix_id = 1
     for cluster in list_of_clusters:
         try:
             poly_o = ransac_fit.polyfit(cluster, 2, 1, disable_linear=False, directory_mode=False)
@@ -64,28 +70,32 @@ for file in file_library:
             y = poly_o["model"].predict(x)
             x = [(item[0]) for item in x]
 
-            #swap the x and y predicted if they were intially swapped by polyfit
+            # swap the x and y predicted if they were intially swapped by polyfit
             if (poly_o["swapped"] == True):
                 tmp = x
                 x = y
                 y = tmp
 
-            #image option
+            if len(x) < min_part:
+                continue
+
+            # image option
             ax1 = None
             if (args.image == 1):
                 ax1 = fig1.gca()
                 ax1.plot(x, y, color="purple")
                 ax1.scatter(cluster[:, 0], cluster[:, 1])
 
-            df2 = pd.DataFrame({'x': x, 'y': y, "box_size": box_size, "box_size_2": box_size, "mode": -3})
+            df2 = pd.DataFrame({'x': x, 'y': y, "box_size": box_size, "box_size_2": box_size, "helix_id": helix_id})
             df = df.append(df2)
+            helix_id += 1
         except ValueError as e:
             print(e)
             continue
-    #image option
+    # image option
     if (args.image == 1):
         ax1.set_title(file)
-        fig1.savefig(args.save_PATH+"/"+file.replace(".txt", ".png"))
-    df.to_csv(args.save_PATH+"/"+file.replace(".txt", ".box"), sep = "\t", index = False, header=False)
+        fig1.savefig(args.save_PATH + "/" + file.replace(".txt", ".png"))
+    df.to_csv(args.save_PATH + "/" + file.replace(".txt", ".box"), sep="\t", index=False, header=False)
 
 
