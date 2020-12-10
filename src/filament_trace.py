aa74474ee0ab824f1b84a57bba87d736e76030ed
Zivvvo <zhe.fan@mail.mcgill.ca>
2020-12-10 00:06:40 -0700

    added GUI for filament_trace, added visualize_file implementations

--- src/filament_trace.py
+++ src/filament_trace.py
@@ -2,63 +2,77 @@ import os
 import sys
 import argparse
 import multiprocessing as mp
-
+from gooey import Gooey
 import warnings
-
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-sys.path.append(".")
-
 import matplotlib.pyplot as plt
 import pandas as pd
+from functools import partial
 
+sys.path.append(".")
 from utils.parsers import *
 from utils.filament_fit import *
 
-sys.path.append(".")
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-parser = argparse.ArgumentParser(
-    description="Traces coordinates along filaments, spaced equidistantly after every given pixel value")
+@Gooey
 
-parser.add_argument("filament_PATH", help="location of filament files")
+def main():
+    sys.path.append(".")
 
-parser.add_argument("-t", "--threshold", nargs= '?', const= -3, help="threshold for particle extraction", type=float)
+    parser = argparse.ArgumentParser(
+        description="Traces coordinates along filaments, spaced equidistantly after every given pixel value")
 
-parser.add_argument("save_PATH", help="save location for fitted filament plots")
+    parser.add_argument("filament_PATH", help="location of filament files")
 
-parser.add_argument("spacing", help="distance between neighbouring particles", type=float)
+    parser.add_argument("-t", "--threshold", nargs= '?', const= -3, help="threshold for particle extraction", type=float)
 
-parser.add_argument("-eps", "--eps", nargs='?', const=10, help="The maximum distance between two " +
-                                                               "samples for one to be considered " +
-                                                               "as in the neighborhood of the other. "
-                    , type=float)
-parser.add_argument("-im", "--image", nargs="?", const=0,
-                    help="if specified with 1, will save an image for the coordiantes as png as well",
-                    type=bool)
-parser.add_argument("-min_samples", "--min_samples", nargs="?", const=5,
-                    help="The number of samples (or total weight) in a neighborhood for a point to be considered as a core point. This includes the point itself."
-                    , type=float)
-parser.add_argument("-box", "--box_size", nargs="?", const=100, help="box size of the selected particles", type=int)
-parser.add_argument("-min_part", "--min_part", nargs="?", const=10,
-                    help="the minimum number of particles in a cluster/filament", type=int)
-parser.add_argument("-processors", "--processors", nargs="?", const= 2, help="Number of processors to use", type=int)
+    parser.add_argument("save_PATH", help="save location for fitted filament plots")
 
-args = parser.parse_args()
+    parser.add_argument("spacing", help="distance between neighbouring particles", type=float)
 
-box_size = args.box_size
+    parser.add_argument("-eps", "--eps", nargs='?', const=10, help="The maximum distance between two " +
+                                                                   "samples for one to be considered " +
+                                                                   "as in the neighborhood of the other. "
+                        , type=float)
+    parser.add_argument("-im", "--image", nargs="?", const=0,
+                        help="if specified with 1, will save an image for the coordiantes as png as well",
+                        type=bool)
+    parser.add_argument("-min_samples", "--min_samples", nargs="?", const=5,
+                        help="The number of samples (or total weight) in a neighborhood for a point to be considered as a core point. This includes the point itself."
+                        , type=float)
+    parser.add_argument("-box", "--box_size", nargs="?", const=100, help="box size of the selected particles", type=int)
+    parser.add_argument("-min_part", "--min_part", nargs="?", const=10,
+                        help="the minimum number of particles in a cluster/filament", type=int)
+    parser.add_argument("-processors", "--processors", nargs="?", const= 2, help="Number of processors to use", type=int)
 
-min_part = args.min_part
+    args = parser.parse_args()
 
-# convert to numpy
-file_library = None
-if args.threshold is not None:
-    file_library = parse_helix_coordinates(args.filament_PATH, threshold=args.threshold)
-    print("args.threshold is provided")
-else:
-    file_library = parse_helix_coordinates(args.filament_PATH)
+    box_size = args.box_size
+
+    min_part = args.min_part
+
+    # convert to numpy
+    file_library = None
+    if args.threshold is not None:
+        file_library = parse_helix_coordinates(args.filament_PATH, threshold=args.threshold)
+        print("args.threshold is provided")
+    else:
+        file_library = parse_helix_coordinates(args.filament_PATH)
+
+    pool = mp.Pool(args.processors)
+
+    func = partial(process_file, file_library, args)
+
+    results = pool.map(func, [file for file in file_library])
+
+    pool.close()
+
+    pool.join()
+
+    exit(100)
 
 
-def process_file(file):
+def process_file(file_library, args, file):
     print("Writing filament coordinates for " + file)
     img = file_library[file]
     plt.scatter(img[0], img[1], marker=".")
@@ -93,7 +107,7 @@ def process_file(file):
                 x = y
                 y = tmp
 
-            if len(x) < min_part:
+            if len(x) < args.min_part:
                 continue
 
             # image option
@@ -103,7 +117,7 @@ def process_file(file):
                 ax1.plot(x, y, color="purple")
                 ax1.scatter(cluster[:, 0], cluster[:, 1])
 
-            df2 = pd.DataFrame({'x': x, 'y': y, "box_size": box_size, "box_size_2": box_size, "helix_id": helix_id})
+            df2 = pd.DataFrame({'x': x, 'y': y, "box_size": args.box_size, "box_size_2": args.box_size, "helix_id": helix_id})
             df = df.append(df2)
             helix_id += 1
         except ValueError as e:
@@ -118,13 +132,4 @@ def process_file(file):
 
 
 if __name__ == "__main__":
-
-    pool = mp.Pool(args.processors)
-
-    results = pool.map(process_file, [file for file in file_library])
-
-    pool.close()
-
-    pool.join()
-
-    exit(100)
+    main()
--- src/visualize_file.py
+++ src/visualize_file.py
@@ -3,57 +3,119 @@ import os
 
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import glob
 import os
 from PIL import Image
 from topaz.utils.data.loader import load_image
 from matplotlib.patches import Circle
+from gooey import Gooey
 
-parser = argparse.ArgumentParser()
+@Gooey
+def main():
+    parser = argparse.ArgumentParser()
 
-parser.add_argument("PATH", help="The specified path for the micrographs, including PATH")
-parser.add_argument("PLOTS_PATH", help = "The specified path for the fitted plot coordinates")
-parser.add_argument("--r", '--radius', help = "The radius of particles")
+    parser.add_argument("micrographs",
+                        help="The specified path for the micrograph you want to visualize, including its name")
+    parser.add_argument("topaz_predictions",
+                        help="The specified path to the topaz coordinate file you have generated from training")
+    parser.add_argument("--r", '--radius', help="The radius of particles", const=15)
+    parser.add_argument("train_test_split_files", help="path to the train test split files generated from topaz")
 
-parser.add_argument("--s", "--SAVE_PATH", help="If specified, will save the file to the specified location")
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    predicted_particles = pd.read_csv(args.topaz_predictions, sep='\t')
 
-micrographs = {}
-for path in glob.glob(args.PATH):
-    im = np.array(load_image(path), copy=False)
-    name,_ = os.path.splitext(os.path.basename(path))
-    micrographs[name] = im
+    _ = plt.hist(predicted_particles.score, bins=50)
+    plt.xlabel('Predicted score (predicted log-likelihood ratio)')
+    plt.ylabel('Number of particles')
+    plt.show()
 
-Continue = True
-while (Continue):
+    proba = 1.0 / (1.0 + np.exp(-predicted_particles.score))
+    _ = plt.hist(proba, bins=50)
+    plt.xlabel('Predicted probability of y=1')
+    plt.ylabel('Number of particles')
+    plt.show()
 
-    name = input("Enter the name of the microtubule file, do not include any file extensions (e.g. .mrc")
+    threshold = input("Enter a threshold value you would like to test")
+    print("Number of particles above threshold:" + str(np.sum(predicted_particles.score >= float(threshold))))
 
-    print("visualization of filament fitting results for " + name)
+    ## load the micrographs for visualization
+    micrographs = {}
+    for path in glob.glob(args.micrographs):
+        im = np.array(load_image(path), copy=False)
+        name, _ = os.path.splitext(os.path.basename(path))
+        micrographs[name] = im
+
+    ## load the train/test split so we can look at results on test set only!
+    images_train = pd.read_csv(os.path.join(args.train_test_split, "image_list_train.txt"), sep='\t')
+    images_train = set(images_train.image_name)
+
+    images_test = pd.read_csv(os.path.join(args.train_test_split, "image_list_test.txt"), sep='\t')
+    images_test = set(images_test.image_name)
+    image_names = list(images_test)  # micrograph names for the test set
+
+    proceed = True
+    while(proceed):
+        name = input("Name of the file you would like to visualize:")
+        radius = input("Radius of particle size you would like to visualize")
+        lowerbound = input("Lower bound of the threshold")
+        upperbound = input("Upper bound of the threshold")
+        visualizer(micrographs, predicted_particles, name, radius, lowerbound, upperbound)
+
+        response = input("Visualize more micrographs? (Y/N)")
+        if response == "Y" or response == "y" or response == "yes" or response == "Yes":
+            continue
+        else:
+            proceed = False
+
+
+def visualizer(micrographs, predicted_particles, name, radius = 15, lowerbound=-1000, upperbound=1000):
+    name = "Map9_00041_3-2"
+    print(name)
     im = micrographs[name]
+    raw_particles = predicted_particles.loc[predicted_particles['image_name'] == name]
 
     # visualize predicted particles with log-likelihood ratio >= 0 (p >= 0.5)
 
-    _,ax = plt.subplots(figsize=(16,16))
+    threshold = -5
+    particles = raw_particles.loc[raw_particles['score'] >= lowerbound].loc[raw_particles['score'] <= upperbound]
+
+    _, ax = plt.subplots(figsize=(16, 16))
     ax.imshow(im, cmap='Greys_r', vmin=-3.5, vmax=3.5, interpolation='bilinear')
 
-    radius = args.radius
+    radius = radius
 
-    file_path = args.PLOTS_PATH + "/" + name + ".csv"
-    df = pd.read_csv(file_path)
+    threshold_steps = 25
+
+    colours = matplotlib.cm.get_cmap('viridis', threshold_steps)
+
+    threshold_vals = np.linspace(raw_particles["score"].min(), raw_particles["score"].max(), threshold_steps)
+
+    # Normalizde the threshold_vals
+
+    i = 0
+    c_map = {}
+    for col in colours.colors:
+        c_map[threshold_vals[i]] = col
+        i = i + 1
 
     # plot the predicted particles in blue
-    for x,y in zip(df["x"],df["y"]):
-        c = Circle((x,y),radius,fill=False,color='b')
+    for x, y, score in zip(particles.x_coord, particles.y_coord, particles.score):
+        # if (score <= 1.5): continue;
+        index = find_nearest(threshold_vals, score)
+        c = Circle((x, y), radius, fill=False, color=c_map[index])
         ax.add_patch(c)
 
-    if (args.SAVE_PATH != None):
-        ax.savefig("--SAVE_PATH")
-
-    response = input("Continue? (y/n)")
-    if (response == "n"):
-        Continue = False
+    plt.show()
+    plt.close()
 
 
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+if __name__ == "__main__":
+    main()
