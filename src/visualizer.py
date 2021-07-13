2dc48286b73a0b0a1e7d28a21466abf37e47c250
Zivvvo <zhe.fan@mail.mcgill.ca>
2021-07-13 16:58:44 -0400

    fixed code for comparing with ground_truth

--- src/visualizer.py
+++ src/visualizer.py
@@ -21,24 +21,28 @@ parser.add_argument('directory', default = "", help = "directory to the box file
 parser.add_argument('--prefix', default = "", help = "prefix of the filename")
 parser.add_argument('--suffix', default = "", help = "suffix of the filename")
 parser.add_argument('--ground_truth', help = "path to the box_file that acts as a reference")
+parser.add_argument('--s' , default = None, help = "if set, will save output image to designated path")
 
 parser.add_argument('--assort_color', default = 0, help = "set to 1 if you want helical groups to be displayed in different colors", type = bool)
 
 ## load the micrographs for visualization
 
+
+
 args = parser.parse_args()
 
 name = args.filename
+
+im = np.array(load_image(name))
+
+name = os.path.splitext(os.path.basename(name))
 prefix = args.prefix
 suffix = args.suffix
 binfactor= args.binfactor
 directory = args.directory
 
-ground_truth = args.ground_truth
+ground_truth = os.path.splitext(args.ground_truth)
 assort_color = args.assort_color
-
-
-im = np.array(load_image(name))
 
 def rebin(arr, new_shape):
     shape = (new_shape[0], arr.shape[0] // new_shape[0],
@@ -48,7 +52,7 @@ def rebin(arr, new_shape):
 
 def get_coordinates(directory, name):
     df = pd.read_csv(os.path.join(directory,
-                                  os.path.basename(name).replace(".mrc",".box").replace(prefix, "").replace(suffix, "")),
+                                  (name+".box").replace(prefix, "").replace(suffix, "")),
                      delimiter = "\t", header = None)
     print(df)
     return (df[0]/binfactor, df[1]/binfactor)
@@ -78,3 +82,6 @@ plt.gca().set_aspect('equal', adjustable='box')
 #plt.gca().invert_yaxis()
 
 plt.show()
+if (args.s is not None):
+    plt.savefig(os.path.join(args.s, os.path.basename(name).replace(".mrc", ""), format = "png"))
+plt.close()
