67cdff9557dda726cd013dbf7e81e683e03ede26
Zivvvo <zhe.fan@mail.mcgill.ca>
2021-07-21 16:43:29 -0400

    finalized visualize_bulk.py

--- src/visualizer_bulk.py
+++ src/visualizer_bulk.py
@@ -11,6 +11,7 @@ import glob
 import os
 import argparse
 from sklearn.neighbors import KDTree
+from matplotlib.patches import Circle
 
 parser = argparse.ArgumentParser(description="Selects a micrograph, finds corresponding box file and ground_truth file, and plots coordinates using"
                                              "the bin factor provided")
@@ -25,7 +26,7 @@ parser.add_argument('--radius', default = 20, help="radius for overlapped points
 parser.add_argument('--assort_color', default = 0, help = "set to 1 if you want helical groups to be displayed in different colors", type = bool)
 #parser.add_argument('--clean_prefix', default = True, help = "remove prefixes on micrograph name, typically due to cryosparc processing", type = bool)
 #parser.add_argument('--clean_suffix', default = True, help = "remove suffixes on micrograph name, typically due to cryosparc processing", type = bool)
-
+parser.add_argument('--diameter', default = 100, help = "diameter of the filament", type = int)
 
 ## load the micrographs for visualization
 
@@ -57,7 +58,6 @@ def get_coordinates(directory, name):
     name = name + ".box"
     df = pd.read_csv(os.path.join(directory,name),
                      delimiter = "\t", header = None)
-    print(df)
     return (df[0]/binfactor, df[1]/binfactor)
 
 def overlapped_points(Truth, Predictions, radius):
@@ -66,14 +66,18 @@ def overlapped_points(Truth, Predictions, radius):
     for point in Predictions:
         point_copy = point[np.newaxis, :]
         ind = tree.query_radius(point_copy, r = 15)
-        if len(ind) != 0:
+        if ind[0].shape[0] != 0:
             output.append(point)
     return output, len(output)
 
 images = []
+index = 0
 for image in glob.glob(os.path.join(image_dir,'*.mrc')):
     im = np.array(load_image(image))
     images.append(im)
+    index = index+1
+    if (index == 10):
+        break
 
 while True:
     try:
@@ -88,7 +92,10 @@ while True:
         biny = round(Ny / binfactor)
         im_bin = rebin(im, (binx, biny))
 
-        plt.imshow(im_bin, cmap="gray")
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+
+        ax.imshow(im_bin, cmap="gray")
 
         box_file_name = find_matching_box(directory, image_name)
 
@@ -96,18 +103,21 @@ while True:
         x = get_coordinates(directory, box_file_name)[0]
         # y = get_coordinates("/storage2/labusr/20210402_TetraCU428/P10/J22/", name)[1]
         y = get_coordinates(directory, box_file_name)[1]
-        plt.scatter(x, y, marker='.', c='b')
+
+
+        print(np.concatenate((np.asarray(x)[:, np.newaxis], np.asarray(y)[:, np.newaxis]), axis = 1))
+
+        ax.scatter(x, y, s = int(args.diameter), edgecolors='b', facecolors = 'none')
 
         if (ground_truth_directory is not None):
             truth_box_file_name = find_matching_box(ground_truth_directory, image_name)
 
             x_g = get_coordinates(ground_truth_directory, truth_box_file_name)[0]
             y_g = get_coordinates(ground_truth_directory, truth_box_file_name)[1]
-            plt.scatter(x, y, marker=".", c='r')
+            ax.scatter(x_g, y_g, s = int(args.diameter), edgecolors='r', facecolors = 'none')
+
 
             truth = pd.concat([x_g, y_g], axis=1)
-            print(truth)
-            print(truth.shape)
             prediction = pd.concat([x, y], axis=1)
 
             truth = truth.to_numpy()
@@ -115,17 +125,21 @@ while True:
             true_positives, _length = overlapped_points(truth, prediction, int(args.radius))
             overlapped_np = np.asarray(true_positives)
 
+            print(overlapped_np)
+
             print("Recall score: " + str(float(_length/truth.shape[0])))
             print("Accuracy score: " + str(float(_length/prediction.shape[0])))
 
-            plt.scatter(overlapped_np[:, 0], overlapped_np[:, 1], c='g')
+            ax.scatter(overlapped_np[:, 0], overlapped_np[:, 1], s= int(args.diameter), edgecolors='y', facecolors = 'none')
+
+
 
         plt.gca().set_aspect('equal', adjustable='box')
         # plt.gca().invert_yaxis()
 
         plt.show()
         if (args.s is not None):
-            plt.savefig(os.path.join(args.s, os.path.basename(image_name).replace(".mrc", ""), format="png"))
+            ax.savefig(os.path.join(args.s, os.path.basename(image_name).replace(".mrc", ""), format="png"))
         plt.close()
 
         Response = input("Continue? Y/N")
